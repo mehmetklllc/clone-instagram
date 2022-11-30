56175892767165299d8674562062320975de4242
mkilic <mehmetklllllc@gmail.com>
2022-11-30 17:52:13 +0300

    first commit for backend

--- cloneinstagramjava/src/main/java/com/instagram/CloneinstagramjavaApplication.java
+++ cloneinstagramjava/src/main/java/com/instagram/CloneinstagramjavaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CloneinstagramjavaApplication {
 
+    //test
     public static void main(String[] args) {
         SpringApplication.run(CloneinstagramjavaApplication.class, args);
     }
