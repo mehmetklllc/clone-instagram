ddb2c088ae62aab3e5e42b8710492d1534c3fd94
mkilic <mehmetklllllc@gmail.com>
2022-12-01 14:40:15 +0300

    axios added for signup page

--- cloneinstagramreact/src/UserSignupPage.js
+++ cloneinstagramreact/src/UserSignupPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import axios from "axios";
 class UserSignupPage extends React.Component {
     state = {
         username: null, displayName: null, password: null, passwordRepeat: null
@@ -13,30 +13,41 @@ class UserSignupPage extends React.Component {
         });
     }
 
+    onClickSignup = event => {
+        event.preventDefault();
+        const {username, displayName, password} = this.state;
+        const body = {
+            username,
+            displayName,
+            password
+        }
+   axios.post('http://localhost:8080/api/create-user',body);
+    };
+
     render() {
 
         return (<form>
-                <h1>Sign Up</h1>
-                <div>
-                    <label>Username : </label>
-                    <input name="username" onChange={this.onChange}/>
-                </div>
-                <div>
-                    <label>Display Name: </label>
-                    <input name="displayName" onChange={this.onChange}/>
-                </div>
-                <div>
-                    <label>Password : </label>
-                    <input type="password" name="password" onChange={this.onChange}/>
-                </div>
-                <div>
-                    <label>Password Repeat : </label>
-                    <input type="password" name="passwordRepeat" onChange={this.onChange}/>
-                </div>
-                <button>Sing Up</button>
+            <h1>Sign Up</h1>
+            <div>
+                <label>Username : </label>
+                <input name="username" onChange={this.onChange}/>
+            </div>
+            <div>
+                <label>Display Name: </label>
+                <input name="displayName" onChange={this.onChange}/>
+            </div>
+            <div>
+                <label>Password : </label>
+                <input type="password" name="password" onChange={this.onChange}/>
+            </div>
+            <div>
+                <label>Password Repeat : </label>
+                <input type="password" name="passwordRepeat" onChange={this.onChange}/>
+            </div>
+            <button onClick={this.onClickSignup}>Sing Up</button>
 
 
-            </form>);
+        </form>);
     }
 }
 
