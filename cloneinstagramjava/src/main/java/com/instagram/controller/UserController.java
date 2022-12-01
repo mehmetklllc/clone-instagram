b4548235a0287a53c05cf678cd4e6838fce86fb7
mkilic <mehmetklllllc@gmail.com>
2022-12-01 14:50:42 +0300

    axios and proxy added for signup page

--- cloneinstagramjava/src/main/java/com/instagram/controller/UserController.java
+++ cloneinstagramjava/src/main/java/com/instagram/controller/UserController.java
@@ -3,7 +3,6 @@ package com.instagram.controller;
 import com.instagram.dto.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +13,6 @@ public class UserController {
 
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
-    @CrossOrigin
     @PostMapping("/api/create-user")
     public void createUser(@RequestBody User user) {
         log.info(user.toString());
