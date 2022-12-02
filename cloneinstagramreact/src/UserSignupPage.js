46008e82c2034a49680f2797eed31e22ab0fbca6
mkilic <mehmetklllllc@gmail.com>
2022-12-02 15:44:57 +0300

    added bootstrap-override.scss for react project

--- cloneinstagramreact/src/UserSignupPage.js
+++ cloneinstagramreact/src/UserSignupPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+
 class UserSignupPage extends React.Component {
     state = {
         username: null, displayName: null, password: null, passwordRepeat: null
@@ -21,33 +22,43 @@ class UserSignupPage extends React.Component {
             displayName,
             password
         }
-   axios.post('/api/create-user',body);
+        axios.post('/api/create-user', body);
     };
 
     render() {
 
-        return (<form>
-            <h1>Sign Up</h1>
-            <div>
-                <label>Username : </label>
-                <input name="username" onChange={this.onChange}/>
-            </div>
-            <div>
-                <label>Display Name: </label>
-                <input name="displayName" onChange={this.onChange}/>
-            </div>
-            <div>
-                <label>Password : </label>
-                <input type="password" name="password" onChange={this.onChange}/>
-            </div>
-            <div>
-                <label>Password Repeat : </label>
-                <input type="password" name="passwordRepeat" onChange={this.onChange}/>
-            </div>
-            <button onClick={this.onClickSignup}>Sing Up</button>
+        return (
+            <div className="border border-3 rounded ">
+                <div className="container">
+                    <form>
+                        <h1 className="text-center">Instagram</h1>
+                        <h1 className="text-center">Sign Up</h1>
+                        <div className="mb-3">
+                            <label className="form-label">Username : </label>
+                            <input className="form-control" name="username" onChange={this.onChange}/>
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Display Name: </label>
+                            <input className="form-control" name="displayName" onChange={this.onChange}/>
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Password : </label>
+                            <input className="form-control" type="password" name="password" onChange={this.onChange}
+                                   aria-describedby="passwordHelpBlock"/>
+                        </div>
 
+                        <div className="mb-3">
+                            <label className="form-label">Password Repeat : </label>
+                            <input className="form-control" type="password" name="passwordRepeat"
+                                   onChange={this.onChange}/>
+                        </div>
+                        <div className="text-center">
+                            <button className="btn btn-outline-primary" onClick={this.onClickSignup}>Sing Up</button>
+                        </div>
 
-        </form>);
+                    </form>
+                </div>
+            </div>);
     }
 }
 
